14cdc4c091196ea25bd91275102341e50716b4e7
lukedernstrom <97061481+lukedernstrom@users.noreply.github.com>
2025-08-06 21:27:38 -0700

    Bug fixes and comments
    While adding comments I noticed several bugs to fix and cleaned up some of the logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import math
 
+# Class definition for the node of the tree
 class Node:
     def __init__(self,
                  wins:int,
@@ -35,9 +36,11 @@ class Node:
 #     2 Two of a Kind
 #     1 High Card
 
+# Returns the suit of the given card as a string
 def getSuit(card):
     return card[-1]
 
+# Returns the value of the given card as an int, converting face cards
 def getVal(card):
     val = card[0:-1]
     if val == 'A':
@@ -51,6 +54,8 @@ def getVal(card):
     
     return int(val)
 
+# Given a list of cards, return the 5 card hand that makes the best flush, or False if none exist
+# Returns strings, not values
 def isFlush(hand):
     suitDist = handSuits(hand)
     for suit in suitDist:
@@ -59,38 +64,35 @@ def isFlush(hand):
             for card in hand:
                 if getSuit(card) == suit:
                     scoringHand.append(card)
-            return scoringHand
+            scoringHand.sort(reverse=True)
+            return scoringHand[:5]
     return False
-    # suit = getSuit(hand[0])
-    # return all(suit == getSuit(card) for card in hand[1:-1])
 
+# Given a list of cards, return the high card for the hand that makes the best straight, or False if none exist
 def isStraight(hand):
     run = 1
     ordered= [getVal(card) for card in hand]
     ordered.sort(reverse=True)
-    if ordered[0] == 14:
+    if ordered[0] == 14: #Treats Aces as highest value and lowest value
         ordered.append(1)
     high = ordered[0]
-    for i in range(0,len(ordered)-1):
+    for i in range(len(ordered)-1):
         if ordered[i] == ordered[i+1] + 1:
             run += 1
             if run == 5:
-                return [high]
+                return high
         elif ordered[i] != ordered[i+1]:
             run = 1
             high = ordered[i+1]
     return False
 
+# Returns the high card of the hand that makes a straight flush
 def isStraightFlush(hand):
-    highCard = isStraight(hand)
-    if (highCard and isFlush(hand)):
-        return highCard
+    if (x := isStraight(hand)):
+        return x
     return False
 
-def isRoyalFlush(hand):
-    # total = sum(getVal(card) for card in hand)
-    return(isStraightFlush(hand) == 14)
-
+# Returns a dictionary mapping each appearing card value to the number of times it appears in the hand
 def handVals(hand):
     dist = {}
     for card in hand:
@@ -101,6 +103,7 @@ def handVals(hand):
             dist[val] += 1
     return dist
 
+# Returns a dictionary mapping each suit to the number of times it appears in the hand
 def handSuits(hand):
     dist = {}
     for card in hand:
@@ -111,6 +114,8 @@ def handSuits(hand):
             dist[suit] += 1
     return dist
 
+# Returns a tuple of three lists. Each list contains the card values that appear the given number of times 
+# (i.e. three[] contains the list of cards that appear 3 times)
 def pairs(dist:dict):
     twos = []
     three = []
@@ -124,13 +129,17 @@ def pairs(dist:dict):
             four.append(val)
     return (twos,three,four)
 
+# Returns a tuple score for the hand. The first value is the numeric value of that hand, the second is cards/values to be used in the case of a tie
 def score(hand):
     vals = handVals(hand)
-    straight = isStraight(hand)
     flush = isFlush(hand)
-    if straight and flush:
-        if straight == 14: return (10, [10])
-        else: return (9, straight)
+    # if straight and flush:
+    #     if straight == 14: return (10, [10])
+    #     else: return (9, straight)
+
+    if flush and (strFlush := isStraightFlush(flush)):
+        if strFlush == 14: return (10, [10])
+        else: return (9, strFlush)
     two,three,four = pairs(vals)
 
     # Four of a kind, kicker = highest card not in the 4
@@ -148,12 +157,16 @@ def score(hand):
         three.sort()
         return(7,[three[-1],three[-2]])
     
-    # Flush, scoring hand derived in flush function
+    # Flush
     if flush: 
-        return (6,flush)
+        scoringHand = []
+        for card in flush:
+            scoringHand.append(getVal(card))
+        return (6,flush[:6])
     
     # Scoring hand derived in straight function
-    if straight: return (5,straight)
+    straight = isStraight(hand)
+    if straight: return (5,[straight])
     
     # Three of a kind, kicker = 2 highest cards not in the pair
     if three: 
@@ -165,8 +178,6 @@ def score(hand):
         kicker.sort(reverse=True)
         three.extend(kicker)
         return (4,three)
-    
-
     if two:
         if len(two) >= 2:
         # Two pair, kicker = highest card not in pairs
@@ -188,6 +199,7 @@ def score(hand):
             kicker.sort(reverse=True)
             two.extend(kicker)
             return (2,two)
+        
     scoringHand = []
     while len(scoringHand) < 5:
         highCard = max(vals)
@@ -195,6 +207,7 @@ def score(hand):
         vals.pop(highCard)
     return (1,scoringHand)
 
+# Calculates the score for two hands then compares. First by looking at the hand category, then tiebreakers if necessary.
 def compare(h1, h2):
     score1 = score(h1)
     score2 = score(h2)
@@ -204,8 +217,8 @@ def compare(h1, h2):
         if me < opp:
             return 0
     return 1
-    
 
+#Creates a deck of 52 unique cards as a set by iterating through every possible suit and value
 def buildDeck() -> set:
     vals = ['A','K','Q','J','10','9','8','7','6','5','4','3','2']
     suits = ['S','C','D','H']
@@ -216,6 +229,7 @@ def buildDeck() -> set:
             deck.add(card)
     return deck
 
+# Given a deck and n number of cards, picks n cards, removes them from a copy of the deck, and returns the selected cards along with the new deck 
 def randCards(deck:set, num:int):
     hand = []
     newDeck = deck.copy()
@@ -225,6 +239,7 @@ def randCards(deck:set, num:int):
         hand.append(card)
     return hand, newDeck
 
+# Given a node of the tree, simulates the resulting poker game and updates itself and all preceeding nodes with the outcome
 def backPropagate(node:Node):
     myHand, oppHand = simulate(node)
     outcome = compare(myHand, oppHand)
@@ -235,16 +250,15 @@ def backPropagate(node:Node):
         backProp=backProp.parent
     return
 
+# Selects the best node to explore
 def selection(root:Node):        
     if len(root.comm) == 5:
         backPropagate(root)
         return
-    
     if root.visits == 0:
         branch(root, root.deck)
         backPropagate(root)
         return
-    
     bestScore = 0
     bestNode = root.children[0]
     for child in root.children:
@@ -257,7 +271,8 @@ def selection(root:Node):
             bestNode = child
     selection(bestNode)
     return
-        
+
+# Manual ucb1 calculation
 def ucb1(node:Node):
     w = node.wins
     n = node.visits
@@ -267,18 +282,8 @@ def ucb1(node:Node):
 
     return (w/n) + c * math.sqrt(ln/n)
 
-def estimator(hole: list):
-    deck = buildDeck()
-    for card in hole:
-        deck.remove(card)
-    
-    root = Node(0,0,hole,[],[],deck,[],None)
-    # branch(root, deck)
-
-    for _ in range(20000):
-        selection(root)
-    print(root.wins/ root.visits)
-    
+# Populates the remainder of the environment with randomized information
+# Returns a complete 7 card hand consisting of 2 unique hole cards for the player and opponent and 5 shared community cards
 def simulate(node:Node):
     deck = node.deck.copy()
     opp = node.opp.copy()
@@ -293,7 +298,7 @@ def simulate(node:Node):
     opp.extend(comm)
     return (myHand,opp)
 
-
+# Branches off of the given node to create 1000 unique children
 def branch(parent:Node, deck:set):
     for _ in range(1000):
         if parent.opp == []:
@@ -304,20 +309,27 @@ def branch(parent:Node, deck:set):
             node = Node(0,0,parent.hand.copy(),parent.opp.copy(),flop,newDeck,[],parent)
         elif len(parent.comm) == 3:
             turn, newDeck = randCards(deck, 1)
-            turn.extend(parent.comm)
-            node = Node(0,0,parent.hand.copy(),parent.opp.copy(),turn,newDeck,[],parent)
+            comm = parent.comm + turn
+            node = Node(0,0,parent.hand.copy(),parent.opp.copy(),comm,newDeck,[],parent)
         elif len(parent.comm) == 4:
             river, newDeck = randCards(deck, 1)
-            river.extend(parent.comm)
-            node = Node(0,0,parent.hand.copy(),parent.opp.copy(),river,newDeck,[],parent)
-        
+            comm = parent.comm + river
+            node = Node(0,0,parent.hand.copy(),parent.opp.copy(),comm,newDeck,[],parent)
         parent.children.append(node)
-        # if not done: branch(node, newDeck, done)
     return
 
+def main(hole: list):
+    deck = buildDeck()
+    for card in hole:
+        deck.remove(card)
+    
+    root = Node(0,0,hole,[],[],deck,[],None)
 
+    for _ in range(20000):
+        selection(root)
+    print(root.wins/ root.visits)
     
 
 
 if __name__ == "__main__":
-    estimator(['4H','4C'])
+    main(['4H','4C'])
